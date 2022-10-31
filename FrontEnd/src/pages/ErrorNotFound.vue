04d414e9fa5b1881ec44a5952b25009e9af40ccc
GeneDing <98688017+GeneDing@users.noreply.github.com>
2022-10-31 16:25:20 +0000

    UpdateQuasarApp

--- FrontEnd/src/pages/ErrorNotFound.vue
+++ FrontEnd/src/pages/ErrorNotFound.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-warning text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
-        404
+        404 Not Found
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Sorry, here's nothing
       </div>
 
       <q-btn
