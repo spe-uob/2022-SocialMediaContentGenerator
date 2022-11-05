23e5e1eebb749bc8a1924fc874c3a8a75a6235d1
Flgodd <benjyaram@gmail.com>
2022-11-05 16:47:25 +0000

    Added a simple CNN

--- experiment/exp0x01/SimpleNN.py
+++ experiment/exp0x01/SimpleNN.py
@@ -18,12 +18,34 @@ class NN(nn.Module):
         x = self.fc2(x)
         return x
 
+# simple CNN
+class CNN(nn.Module):
+    def __init__(self, in_channels =1, num_classes = 10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=(1, 1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+
+
+
 # Set device
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
+in_channels = 1
 input_size = 784
 num_classes = 10
 learning_rate = 0.001
@@ -37,7 +59,8 @@ test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 # Initialising network
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device)
+# model = NN(input_size=input_size, num_classes=num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -51,7 +74,7 @@ for epoch in range(num_epochs):
         targets = targets.to(device=device)
 
         # convert to correct shape
-        data = data.reshape(data.shape[0], -1)
+        # data = data.reshape(data.shape[0], -1) <---- already done earlier in program for CNN
 
         # forward
         scores = model(data)
@@ -82,7 +105,7 @@ def check_accuracy(loader, model):
             x = x.to(device=device)
             y = y.to(device=device)
 
-            x = x.reshape(x.shape[0], -1)
+            # x = x.reshape(x.shape[0], -1) <---- already done for CNN
 
             scores = model(x)
             _, predictions = scores.max(1)
