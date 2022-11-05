d37d85c5cfca939754139371c123f518f2e9a67d
GeneDing <98688017+GeneDing@users.noreply.github.com>
2022-11-05 15:50:51 +0000

    UpdateStyle

--- FrontEnd/src/layouts/MainLayout.vue
+++ FrontEnd/src/layouts/MainLayout.vue
@@ -41,20 +41,3 @@ export default {
   }
 }
 
-</script>
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    const rightDrawerOpen = ref(false)
-
-    return {
-      rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
-</script>
