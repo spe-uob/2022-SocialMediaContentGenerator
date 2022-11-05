73600f25eb4bf94f08f4ecec9e0e4822bdcd7aa6
GeneDing <98688017+GeneDing@users.noreply.github.com>
2022-11-05 16:43:18 +0000

    Update style

--- FrontEnd/src/layouts/MainLayout.vue
+++ FrontEnd/src/layouts/MainLayout.vue
@@ -1,21 +1,68 @@
 <template>
-  <q-layout view="hhh lpR lFf">
+  <q-layout view="hhh lpR lFf" container style="height: 900px" class="shadow-2 rounded-borders">
 
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated class="bg-indigo-7 text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="~/assets/drone-svgrepo-com.svg">
           </q-avatar>
-          Title
+           Social Media Content Generator
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
-      <!-- drawer content -->
+    <q-drawer v-model="leftDrawerOpen" side="left"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      overlay bordered
+      class="bg-light-blue-2">
+      <q-scroll-area class="fit">
+                <q-list padding class="menu-list">
+                  <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-icon name="send" />
+                    </q-item-section>
+
+                    <q-item-section>
+                      Item 1
+                    </q-item-section>
+                  </q-item>
+
+                  <q-item active clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-icon name="send" />
+                    </q-item-section>
+
+                    <q-item-section>
+                      Item 2
+                    </q-item-section>
+                  </q-item>
+
+                  <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-icon name="send" />
+                    </q-item-section>
+
+                    <q-item-section>
+                      Item 3
+                    </q-item-section>
+                  </q-item>
+
+                  <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-icon name="send" />
+                    </q-item-section>
+
+                    <q-item-section>
+                      Item 4
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -40,4 +87,4 @@ export default {
     }
   }
 }
-
+</script>
