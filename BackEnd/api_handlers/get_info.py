622e0cf6a55f0e19e3ccd2834f00c0d7a8d9eb08
duskxi <84715902+duskxi@users.noreply.github.com>
2022-11-16 12:14:48 +0000

    change code followed api server init rule

--- BackEnd/api_handlers/get_info.py
+++ BackEnd/api_handlers/get_info.py
@@ -5,10 +5,14 @@ import tornado.web
 
 class GetInfo(tornado.web.RequestHandler):
     def __init__(self, application, request, **kwargs):
+        self.information_object = {}
         super().__init__(application, request, **kwargs)
 
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
         pass
 
+    def initialize(self, information_object):
+        self.information_object = information_object
+
     def get(self):
-        self.write("Hello, world")
+        self.write(self.information_object)
