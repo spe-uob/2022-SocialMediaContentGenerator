a12d8c8c628bdfc5d05c71bc2c69341ab1dc7d32
duskxi <84715902+duskxi@users.noreply.github.com>
2022-11-16 12:12:26 +0000

    optimize logical code, change adaptability and extensibility

--- BackEnd/api_server.py
+++ BackEnd/api_server.py
@@ -5,15 +5,20 @@ import tornado.web
 
 class Application:
     def __init__(self, port, host="0.0.0.0"):
-        self.handlers = []
+        self.components = []
         self.port = port
         self.host = host
 
-    def register_handler(self, handler):
-        self.handlers.append(handler)
+    def register_handler(self, router, handler, params: dict):
+        self.components.append({"class": handler, "params": params, "router": router})
 
     def build_app(self):
-        return tornado.web.Application([(r"/", handler) for handler in self.handlers])
+        return tornado.web.Application(
+            [
+                (component["router"],
+                 component["class"],
+                 component["params"]) for component in self.components
+            ])
 
     async def run(self):
         app = self.build_app()
