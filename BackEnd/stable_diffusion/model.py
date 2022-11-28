2a1f1a86fafe6fa87cd00992c10e08e62d0e37de
duskxi <84715902+duskxi@users.noreply.github.com>
2022-11-28 21:51:32 +0000

    model loader

---
+++ BackEnd/stable_diffusion/model.py
@@ -0,0 +1,26 @@
+import torch
+from ldm.util import instantiate_from_config
+
+
+class Model:
+    def __init__(self):
+        self.model = None
+
+    def load_model_from_config(self, config, ckpt, verbose=False):
+        print(f"Loading model from {ckpt}")
+        pl_sd = torch.load(ckpt, map_location="cpu")
+        if "global_step" in pl_sd:
+            print(f"Global Step: {pl_sd['global_step']}")
+        sd = pl_sd["state_dict"]
+        model = instantiate_from_config(config.model)
+        m, u = model.load_state_dict(sd, strict=False)
+        if len(m) > 0 and verbose:
+            print("missing keys:")
+            print(m)
+        if len(u) > 0 and verbose:
+            print("unexpected keys:")
+            print(u)
+
+        model.cuda()
+        model.eval()
+        self.model = model
