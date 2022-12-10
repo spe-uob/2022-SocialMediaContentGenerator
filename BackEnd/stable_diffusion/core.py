4263bccc575c734dddd90fba0e885893f5387fa6
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-10 23:00:52 +0000

    update device selector

--- BackEnd/stable_diffusion/core.py
+++ BackEnd/stable_diffusion/core.py
@@ -1,3 +1,4 @@
+import torch
 from omegaconf import OmegaConf
 
 from stable_diffusion.model import Model
@@ -10,3 +11,6 @@ class Core:
     def on_load_model(self, model_name):
         config = OmegaConf.load(f"v1-inference.yaml")
         self.model.load_model_from_config(config, model_name, verbose=False)
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        model = self.model.model.to(device)
+        self.model.model = model
