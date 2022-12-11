495823394e86241d491fa2ef88fc694e69132689
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 20:33:48 +0000

    change layout

--- temporary_ui/src/layouts/MainLayout.vue
+++ temporary_ui/src/layouts/MainLayout.vue
@@ -19,25 +19,25 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+<!--    <q-drawer-->
+<!--      v-model="leftDrawerOpen"-->
+<!--      show-if-above-->
+<!--      bordered-->
+<!--    >-->
+<!--      <q-list>-->
+<!--        <q-item-label-->
+<!--          header-->
+<!--        >-->
+<!--          Essential Links-->
+<!--        </q-item-label>-->
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+<!--        <EssentialLink-->
+<!--          v-for="link in essentialLinks"-->
+<!--          :key="link.title"-->
+<!--          v-bind="link"-->
+<!--        />-->
+<!--      </q-list>-->
+<!--    </q-drawer>-->
 
     <q-page-container>
       <router-view />
@@ -98,7 +98,7 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+
   },
 
   setup () {
