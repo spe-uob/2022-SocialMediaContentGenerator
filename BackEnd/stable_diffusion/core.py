21f6f88440efbb2b60ca4812bc8d5a1e5fa00308
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 19:04:23 +0000

    optimize progress

--- BackEnd/stable_diffusion/core.py
+++ BackEnd/stable_diffusion/core.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 from loguru import logger
 from PIL import Image
+from rich.progress import Progress
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from einops import rearrange
 from imwatermark import WatermarkEncoder
@@ -72,42 +73,48 @@ class Core:
                 with model.ema_scope():
                     tic = time.time()
                     all_samples = list()
-                    for n in trange(batch_size, desc="Sampling"):
-                        for prompts in tqdm(data, desc="data"):
-                            uc = None
-                            if cfg != 1.0:
-                                uc = model.get_learned_conditioning(batch_size * [""])
-                            if isinstance(prompts, tuple):
-                                prompts = list(prompts)
-                            c = model.get_learned_conditioning(prompts)
-                            shape = [channel, height // f, width // f]
-                            samples_ddim, _ = sampler.sample(S=step,
-                                                             conditioning=c,
-                                                             batch_size=batch_size,
-                                                             shape=shape,
-                                                             verbose=False,
-                                                             unconditional_guidance_scale=cfg,
-                                                             unconditional_conditioning=uc,
-                                                             eta=ddim_eta,
-                                                             x_T=start_code)
+                    with Progress() as progress:
+                        task1 = progress.add_task("[red]Sampling...", total=batch_size)
+                        for n in range(batch_size):
+                            task2 = progress.add_task("[green]Generating...", total=len(data))
+                            for prompts in data:
+                                uc = None
+                                if cfg != 1.0:
+                                    uc = model.get_learned_conditioning(batch_size * [""])
+                                if isinstance(prompts, tuple):
+                                    prompts = list(prompts)
+                                c = model.get_learned_conditioning(prompts)
+                                shape = [channel, height // f, width // f]
+                                samples_ddim, _ = sampler.sample(S=step,
+                                                                 conditioning=c,
+                                                                 batch_size=batch_size,
+                                                                 shape=shape,
+                                                                 verbose=False,
+                                                                 unconditional_guidance_scale=cfg,
+                                                                 unconditional_conditioning=uc,
+                                                                 eta=ddim_eta,
+                                                                 x_T=start_code)
 
-                            x_samples_ddim = model.decode_first_stage(samples_ddim)
-                            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                            x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                                x_samples_ddim = model.decode_first_stage(samples_ddim)
+                                x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                                x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
 
-                            x_checked_image, has_nsfw_concept = self.check_safety(x_samples_ddim)
+                                x_checked_image, has_nsfw_concept = self.check_safety(x_samples_ddim)
 
-                            x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+                                x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
-                            for x_sample in x_checked_image_torch:
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                img = Image.fromarray(x_sample.astype(np.uint8))
-                                # img = put_watermark(img, wm_encoder)
-                                img.save(os.path.join(self.save_path, f"{base_count:05}.png"))
-                                base_count += 1
+                                for x_sample in x_checked_image_torch:
+                                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                                    img = Image.fromarray(x_sample.astype(np.uint8))
+                                    # img = put_watermark(img, wm_encoder)
+                                    img.save(os.path.join(self.save_path, f"{base_count:05}.png"))
+                                    base_count += 1
 
-                            if not skip_grid:
-                                all_samples.append(x_checked_image_torch)
+                                if not skip_grid:
+                                    all_samples.append(x_checked_image_torch)
+
+                                progress.update(task2, advance=1)
+                            progress.update(task1, advance=1)
 
                     if not skip_grid:
                         # additionally, save as grid
