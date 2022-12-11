a835454edf42d352165e0158df10281375b16a4b
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 17:27:58 +0000

    sampler

--- BackEnd/stable_diffusion/core.py
+++ BackEnd/stable_diffusion/core.py
@@ -1,4 +1,7 @@
 import torch
+from ldm.models.diffusion.ddim import DDIMSampler
+from ldm.models.diffusion.dpm_solver import DPMSolverSampler
+from ldm.models.diffusion.plms import PLMSSampler
 from omegaconf import OmegaConf
 
 from stable_diffusion.model import Model
@@ -14,3 +17,12 @@ class Core:
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         model = self.model.model.to(device)
         self.model.model = model
+
+    def exec_sample(self, sample: str, step=20):
+        model = self.model.model
+        sampler = DDIMSampler(model)
+        if sample.lower() == "dpm":
+            sampler = DPMSolverSampler(model)
+        elif sample.lower() == "plms":
+            sampler = PLMSSampler(model)
+
