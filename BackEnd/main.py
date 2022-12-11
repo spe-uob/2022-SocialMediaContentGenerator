1a3ce5e37964c440a88fdfd813848611a0d3ad50
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 21:20:22 +0000

    instantiate info provider

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -2,6 +2,7 @@ import asyncio
 import argparse
 
 from api_server import Application
+from information_provider import InformationProvider
 from stable_diffusion.core import Core
 from stable_diffusion.model import Model
 from api_reg import api_register
@@ -9,10 +10,13 @@ from api_reg import api_register
 
 def main(opt: argparse.Namespace):
     core = Core()
+    information_provider = InformationProvider(core)
+    test = information_provider.get_info()
     app = Application(8888)
     config = {
         "core": core,
-        "model_dir": opt.model_path
+        "model_dir": opt.model_path,
+        "information_provider": information_provider,
     }
     api_register(app, config)
     app.build_app()
