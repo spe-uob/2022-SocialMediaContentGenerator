97863b8440fb48fef43146e5da30827b9dfe1cbb
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 21:19:10 +0000

    model loader

--- temporary_ui/src/pages/IndexPage.vue
+++ temporary_ui/src/pages/IndexPage.vue
@@ -1,57 +1,84 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="row" style="width: 80%">
-      <div class="col-4">
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Control Area</div>
-          </q-card-section>
-          <q-card-section>
-            <div class="text-h6">Prompt:</div>
-            <q-input
-              v-model="prompts"
-              filled
-              autogrow
-            />
-          </q-card-section>
-          <q-card-section>
-            <q-badge color="secondary">
-              Steps: {{ step }} (0 to 150)
-            </q-badge>
-            <q-slider v-model="step" :min="0" :max="150"/>
-            <q-badge color="warning"> CFG: {{ cfg }} (1 to 30)</q-badge>
-            <q-slider v-model="cfg" :min="1.0" :max="30.0" :step="0.5"/>
-          </q-card-section>
-          <q-card-section>
-            <q-badge color="positive"> width: {{ width }}</q-badge>
-            <q-slider v-model="width" :min="64" :max="2048" :step="64"/>
-            <q-badge color="positive"> height: {{ height }}</q-badge>
-            <q-slider v-model="height" :min="64" :max="2048" :step="64"/>
-          </q-card-section>
-          <q-card-section>
-            <q-btn color="primary" label="generate" :disable="generating" @click="generate"/>
-          </q-card-section>
-        </q-card>
+    <div class="column" style="width: 100vw">
+      <div class="col flex flex-center">
+        <div class="row" style="width: 80%">
+          <div class="col-12 ">
+            <q-card>
+              <q-card-section>
+                <q-select filled bottom-slots v-model="model" :options="models" label="Models" counter maxlength="12" :loading="loading_model" @update:model-value="load_model">
+                  <template v-slot:hint>
+                    model loader
+                  </template>
+                  <template v-slot:after>
+                    <q-btn round dense flat icon="refresh" @click="syncModelList"/>
+                  </template>
+                </q-select>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
       </div>
-      <div class="col-8">
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Image</div>
-            <q-img :src="image" style="max-width: 40vw"></q-img>
-          </q-card-section>
-        </q-card>
+      <div class="col flex flex-center">
+        <div class="row q-pa-xl q-mr-xl" style="width: 80%">
+          <div class="col-4 q-pa-sm">
+            <q-card>
+              <q-card-section>
+                <div class="text-h3">Control Area</div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h6">Prompt:</div>
+                <q-input
+                  v-model="prompts"
+                  filled
+                  autogrow
+                />
+              </q-card-section>
+              <q-card-section>
+                <q-badge color="secondary">
+                  Steps: {{ step }} (0 to 150)
+                </q-badge>
+                <q-slider v-model="step" :min="0" :max="150"/>
+                <q-badge color="warning"> CFG: {{ cfg }} (1 to 30)</q-badge>
+                <q-slider v-model="cfg" :min="1.0" :max="30.0" :step="0.5"/>
+              </q-card-section>
+              <q-card-section>
+                <q-badge color="positive"> width: {{ width }}</q-badge>
+                <q-slider v-model="width" :min="64" :max="2048" :step="64"/>
+                <q-badge color="positive"> height: {{ height }}</q-badge>
+                <q-slider v-model="height" :min="64" :max="2048" :step="64"/>
+              </q-card-section>
+              <q-card-section>
+                <q-btn color="primary" label="generate" :disable="generating" @click="generate"/>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-8 q-pa-sm">
+            <q-card>
+              <q-card-section>
+                <div class="text-h3">Image Show</div>
+              </q-card-section>
+              <q-card-section>
+                <q-img :src="image" style="max-width: 40vw"></q-img>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import {defineComponent, onBeforeUnmount} from 'vue'
+import {defineComponent} from 'vue'
 
 export default defineComponent({
   name: 'IndexPage',
   data() {
     return {
+      model: null,
+      models: [],
+      loading_model: false,
       prompts: '',
       generating: false,
       image: '',
@@ -62,6 +89,9 @@ export default defineComponent({
     }
   },
   methods: {
+    async sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    },
     async generate() {
       this.generating = true
       this.$q.loading.show()
@@ -87,7 +117,57 @@ export default defineComponent({
       console.log("generate complete")
       console.log(response)
       this.generating = false
+    },
+    async syncModelList() {
+      this.loading_model = true
+      await this.sleep(300);
+      let request = await fetch('http://localhost:8888/api/v1/model_list', {method: 'GET', mode: 'cors'});
+      let response = await request.json();
+      console.log(response)
+      this.models = response['mode_list'];
+      this.loading_model = false
+    },
+    async load_model() {
+      this.loading_model = true
+      const dismiss = this.$q.notify({
+        spinner: true,
+        message: `loading model ${this.model}...`,
+        position: "top",
+        timeout: 0,
+      })
+      let request = await fetch(`http://localhost:8888/api/v1/load_model?ModelName=${this.model}`, {method: 'GET', mode: 'cors'});
+      let response = await request.json();
+      dismiss()
+      if (response['status'] === 0)
+        this.$q.notify({
+          spinner: true,
+          message: `loading model ${this.model} complete!`,
+          timeout: 3000,
+          color: "positive",
+          position: "top"
+        })
+      else
+        this.$q.notify({
+          spinner: true,
+          message: `loading model ${this.model} failed!\n ${response['error']}`,
+          timeout: 3000,
+          color: "negative",
+          position: "top"
+        })
+      this.loading_model = false
+    },
+    async getCurrentModel() {
+      let info = await this.getInfo();
+      this.model = info['current_model']
+    },
+    async getInfo() {
+      let request = await fetch('http://localhost:8888/api/v1/get_info', {method: 'GET', mode: 'cors'});
+      return request.json();
     }
+  },
+  async beforeMount() {
+    await this.syncModelList()
+    await this.getCurrentModel();
   },
   beforeUnmount() {
     this.$q.loading.hide()
