cae8ca1ee1848216522e6c7470ebdea86f5ae1bd
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-11 21:19:53 +0000

    setup info provider

--- BackEnd/api_reg.py
+++ BackEnd/api_reg.py
@@ -3,7 +3,7 @@ from api_handlers import *
 
 
 def api_register(app: Application, config: dict):
-    app.register_handler(r"/api/v1/get_info", GetInfo, {"information_object": {}})
+    app.register_handler(r"/api/v1/get_info", GetInfo, {"information_provider": config["information_provider"]})
     app.register_handler(r"/api/v1/model_list", ModelList, {"model_dir": config["model_dir"]})
     app.register_handler(r"/api/v1/load_model", LoadModel, {"on_load_model": config["core"].on_load_model})
     app.register_handler(r"/api/v1/sample", Sample, {"core": config["core"]})
