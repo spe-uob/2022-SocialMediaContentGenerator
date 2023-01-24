4440624a8375048ba17d33d146822250b096e89d
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-01-24 16:33:47 +0000

    Add BasicSpinner

---
+++ FrontEnd/src/components/BasicSpinner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row">
+      <q-spinner
+        color="primary"
+        size="3em"
+        :thickness="10"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicSpinner"
+}
+</script>
+
+<style scoped>
+
+</style>
