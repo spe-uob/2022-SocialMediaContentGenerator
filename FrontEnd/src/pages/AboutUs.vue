194534e528ee248cfeda9363bb241b1a1570778b
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-01-24 17:29:04 +0000

    add new files

---
+++ FrontEnd/src/pages/AboutUs.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="q-pa-sm">
+    <div class="row q-col-gutter-lg">
+      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="data in card_data" :key="data.id">
+        <MemberInfoCard class="col-lg-4 fit col-md-4 col-sm-12 col-xs-12"
+                        :id = "data.id"
+                        :avatar="data.avatar"
+                        :name="data.name"
+                        :disc="data.disc"
+                        :contact="data.contact"
+                        >
+        </MemberInfoCard>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent, defineAsyncComponent} from 'vue'
+const card_data = [
+  {
+    id:1,
+    name: 'Benjamine_Aram',
+    avatar: '~assets/Benjamine_Avatar.png',
+    contact: 'test@gmail.com',
+    disc:'text',
+  },
+  {
+    id:2,
+    name: 'David_Yan',
+    avatar: '~assets/David_Avatar.png',
+    contact: 'test@gmail.com',
+    disc:'text',
+  },
+
+];
+export default defineComponent({
+  name: "AboutUs",
+  components: {
+    MemberInfoCard: defineAsyncComponent(() => import('components/card/MemberInfoCard'))
+  },
+  setup() {
+    return {
+      card_data
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
---
+++ FrontEnd/src/pages/LabStation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-img
+      :src="url"
+      spinner-color="white"
+      style="height: 700px; max-width: 750px"
+    />
+    <q-btn push color="teal" label="Generate Image" @click="refresh" />
+  </div>
+</template>
+
+<script>
+import { ref,defineComponent } from 'vue'
+
+export default defineComponent({
+  name:"LabStation",
+  setup () {
+    const url = ref('https://placeimg.com/500/300/nature')
+
+    return {
+      url,
+      refresh () {
+        url.value = 'https://placeimg.com/500/300/nature?t=' + Math.random()
+      }
+    }
+  }
+})
+</script>
