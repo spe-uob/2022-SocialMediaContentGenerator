4dc70f904edc9f8f830b5209f99a0b0a37d8951c
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-01-25 16:04:20 +0000

    UpdateAboutUs

--- FrontEnd/src/pages/AboutUs.vue
+++ FrontEnd/src/pages/AboutUs.vue
@@ -2,14 +2,7 @@
   <q-page class="q-pa-sm">
     <div class="row q-col-gutter-lg">
       <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="data in card_data" :key="data.id">
-        <MemberInfoCard class="col-lg-4 fit col-md-4 col-sm-12 col-xs-12"
-                        :id = "data.id"
-                        :avatar="data.avatar"
-                        :name="data.name"
-                        :disc="data.disc"
-                        :contact="data.contact"
-                        >
-        </MemberInfoCard>
+        text
       </div>
     </div>
   </q-page>
@@ -37,7 +30,6 @@ const card_data = [
 export default defineComponent({
   name: "AboutUs",
   components: {
-    MemberInfoCard: defineAsyncComponent(() => import('~components/card/MemberInfoCard'))
   },
   setup() {
     return {
