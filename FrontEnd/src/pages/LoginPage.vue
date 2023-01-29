fc827d1ba8805cf4fc4634e7caadd8aa7bed6de3
Flgodd <benjyaram@gmail.com>
2023-01-29 17:43:00 +0000

    input places added and other additions

--- FrontEnd/src/pages/LoginPage.vue
+++ FrontEnd/src/pages/LoginPage.vue
@@ -6,9 +6,31 @@
         <img src="~assets/undraw_flying_drone_u3r2.svg" class="responsive" alt = "login-image" >
       </div>
       <div class="col-12 col-md-6 flex content-center">
-        <q-card>
-          <!-- todo -->
+        <q-card v-bind:style="$q.screen.lt.sm ? {'width': '80%'} : {'width': '50%'} ">
+          <q-card-section>
+            <q-avatar size="103px" class="absolute-center shadow-10">
+              <img src="~assets/undraw_male_avatar_g98d.svg" alt="avatar">
+            </q-avatar>
+          </q-card-section>
+          <q-card-section>
+            <div class="q-pt-lg">
+              <div class="col text-h6 ellipsis flex justify-center"/>
+              <h2 class="text-h2 text-uppercase q-my-none text-weight-regular">Login</h2>
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input label="Username">
+
+              </q-input>
+              <q-input label="Password" type="password">
+
+              </q-input>
+
+            </q-form>
+          </q-card-section>
         </q-card>
+
       </div>
     </div>
   </q-page>
