617ec7d2dce73be97637624fe970111779b724c2
duskxi <84715902+duskxi@users.noreply.github.com>
2023-02-08 00:30:38 +0000

    reg handler

--- BackEnd/api_reg.py
+++ BackEnd/api_reg.py
@@ -6,4 +6,6 @@ def api_register(app: Application, config: dict):
     app.register_handler(r"/api/v1/get_info", GetInfo, {"information_provider": config["information_provider"]})
     app.register_handler(r"/api/v1/model_list", ModelList, {"model_dir": config["model_dir"]})
     app.register_handler(r"/api/v1/load_model", LoadModel, {"on_load_model": config["core"].on_load_model})
+    app.register_handler(r"/api/v1/load_vae", LoadVAE, {"on_load_vae": config["core"].on_load_vae})
+    app.register_handler(r"/api/v1/vae_list", VAEList, {"model_dir": config["model_dir"]})
     app.register_handler(r"/api/v1/sample", Sample, {"core": config["core"]})
