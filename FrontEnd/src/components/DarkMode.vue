06a50db1e7b18ef48da88e18f3dea5f9782ec788
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-02-08 12:02:57 +0000

    fix bugs for darkmode setting

--- FrontEnd/src/components/DarkMode.vue
+++ FrontEnd/src/components/DarkMode.vue
@@ -1,34 +1,9 @@
 <template>
-<div>
-  <q-toggle
-    size="xl"
-    dark color="teal"
-    keep-color
-    left-label
-    v-model="formData.theme"
-    false-value="light mode"
-    true-value="dark mode"
-    label="theme"
-    icon="moon"
-  />
-
-</div>
-
+?
 </template>
 
 <script>
 import { defineComponent } from "vue";
-
-export default {
-  name: "DarkMode",
-  data(){
-    return{
-      formData: {
-        theme: false
-      }
-    }
-  }
-}
 </script>
 
 <style scoped>
