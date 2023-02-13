6cbf4be6b42a6c972660d928ae10c2ba66b5c6f7
duskxi <84715902+duskxi@users.noreply.github.com>
2023-02-13 18:52:02 +0000

    update

--- BackEnd/sd_rebuild/sampler.py
+++ BackEnd/sd_rebuild/sampler.py
@@ -7,7 +7,7 @@ class TypicalStableDiffusionSampler:
         self.init_latent = None
         self.last_latent = None
         self.step = 0
-        self.eta = None
+        self.eta = 0.0
 
     def sample(self, x, conditioning, unconditional_conditioning, steps, cfg_scale=7.5):
         self.init_latent = None
@@ -17,3 +17,7 @@ class TypicalStableDiffusionSampler:
         sample_results = self.sampler.sample(S=steps, conditioning=conditioning, batch_size=int(x.shape[0]), shape=x[0].shape, verbose=False, unconditional_guidance_scale=cfg_scale,
                                              unconditional_conditioning=unconditional_conditioning, x_T=x, eta=self.eta)[0]
         return sample_results
+
+    @staticmethod
+    def number_of_needed_noises(p):
+        return 0
