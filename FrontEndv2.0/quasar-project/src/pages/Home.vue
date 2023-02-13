cb709232db961dce5fadd75e0ea45ea2a561dda7
Flgodd <benjyaram@gmail.com>
2023-02-13 15:16:56 +0000

    Added an embedded twitter timeline for SpaceNXT on main website

--- FrontEndv2.0/quasar-project/src/pages/Home.vue
+++ FrontEndv2.0/quasar-project/src/pages/Home.vue
@@ -7,9 +7,14 @@
   <div class="col-0 col-md-6 flex justify-center content-center">
     <img src="~assets/spaceNXT.png" class="responsive" alt = "login-image" >
   </div>
-  <a class="twitter-timeline" data-width="1200" data-theme="dark" href="https://twitter.com/SpacenxtLabs?ref_src=twsrc%5Etfw">
-    Tweets by SpacenxtLabs
-  </a>
+  <div class="embedded-twitter-feed">
+    <a class="twitter-timeline" data-width="1200" data-theme="dark" href="https://twitter.com/SpacenxtLabs?ref_src=twsrc%5Etfw">
+      Tweets by SpacenxtLabs
+    </a>
+
+
+
+  </div>
   <!--<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
 
 </template>
@@ -18,6 +23,22 @@
 export default {
   name: "Home",
 }
+window.twttr = (function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
 
 </script>
 
