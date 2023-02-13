4fd4c269a91927541f96101949b5e6cd1649d15c
Flgodd <benjyaram@gmail.com>
2023-02-13 13:10:06 +0000

    Added an embedded twitter timeline for SpaceNXT

--- FrontEndv2.0/quasar-project/src/pages/Home.vue
+++ FrontEndv2.0/quasar-project/src/pages/Home.vue
@@ -7,13 +7,18 @@
   <div class="col-0 col-md-6 flex justify-center content-center">
     <img src="~assets/spaceNXT.png" class="responsive" alt = "login-image" >
   </div>
+  <a class="twitter-timeline" data-width="1200" data-theme="dark" href="https://twitter.com/SpacenxtLabs?ref_src=twsrc%5Etfw">
+    Tweets by SpacenxtLabs
+  </a>
+  <!--<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
 
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
 }
+
 </script>
 
 <style scoped>
