b5740b8bbddd5264cfd8059a0425bbbac16c4da3
duskxi <84715902+duskxi@users.noreply.github.com>
2023-02-15 16:20:34 +0000

    add image upload method

--- FrontEndv2.0/quasar-project/src/pages/Twitter.vue
+++ FrontEndv2.0/quasar-project/src/pages/Twitter.vue
@@ -7,43 +7,88 @@
       class="q-pa-md"
       autogrow
       maxlength="100"
-      >
+    >
       <template v-slot:append>
-            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+        <q-icon name="close" @click="text = ''" class="cursor-pointer"/>
       </template>
       <template v-slot:after>
-            <q-icon name="send" @click="addNewTweet" color="primary" class="cursor-pointer" :disable="!text" />
+        <q-icon name="send" @click="addNewTweetPost" color="primary" class="cursor-pointer" :disable="!text"/>
       </template>
     </q-input>
+    <q-img :src="image" style="max-width: 40vw"></q-img>
+    <q-file color="purple-12" @update:model-value="uploadImage($event)" label="Add images">
+      <template v-slot:prepend>
+        <q-icon name="attach_file"/>
+      </template>
+    </q-file>
   </div>
 </template>
 
 <script>
-import { data } from 'browserslist';
-import { defineComponent } from 'vue'
+import {data} from 'browserslist';
+import {defineComponent} from 'vue'
 import AuthComponent from '../components/AuthComponent.vue'
 
 export default defineComponent({
   name: "Twitter",
   data() {
     return {
-      text: ''
+      text: '',
+      image: ''
     }
   },
   methods: {
     async addNewTweet() {
-    console.log(AuthComponent.data())
-    let tweet = this.text
-    const url = `http://localhost:5000/tweet?status=${encodeURIComponent(tweet)}&consumer_key=${encodeURIComponent(AuthComponent.data().apiKey)}&consumer_secret=${encodeURIComponent(AuthComponent.data().apiSecret)}&access_token=${encodeURIComponent(AuthComponent.data().token)}&access_token_secret=${encodeURIComponent(AuthComponent.data().secret)}`
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
+      console.log(AuthComponent.data())
+      let tweet = this.text
+      const url = `/tweet?status=${encodeURIComponent(tweet)}&consumer_key=${encodeURIComponent(AuthComponent.data().apiKey)}&consumer_secret=${encodeURIComponent(AuthComponent.data().apiSecret)}&access_token=${encodeURIComponent(AuthComponent.data().token)}&access_token_secret=${encodeURIComponent(AuthComponent.data().secret)}&image=${encodeURIComponent(this.image)}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
 
-    let newTweet = {
-    content: this.text,
-      date: Date.now()
-    }
-    this.text = ''
+      let newTweet = {
+        content: this.text,
+        date: Date.now()
+      }
+      this.text = ''
+    },
+    async addNewTweetPost(){
+      let tweet = this.text
+      const url = `/api/v1/tweet`
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          status: tweet,
+          consumer_key: AuthComponent.data().apiKey,
+          consumer_secret: AuthComponent.data().apiSecret,
+          access_token: AuthComponent.data().token,
+          access_token_secret: AuthComponent.data().secret,
+          image: this.image
+        }),
+        mode: 'cors',
+      })
+      const data = await response.json()
+      console.log(data)
+
+      let newTweet = {
+        content: this.text,
+        date: Date.now()
+      }
+      this.text = ''
+    },
+    getBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = error => reject(error)
+      })
+    },
+    async uploadImage(files) {
+      this.image = await this.getBase64(files)
     },
   }
 })
