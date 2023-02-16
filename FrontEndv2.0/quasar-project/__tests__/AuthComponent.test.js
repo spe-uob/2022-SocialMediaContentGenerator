6816032c7400199d2905a4cdd691525d11981e2c
davidhaoyan <60042375+davidhaoyan@users.noreply.github.com>
2023-02-16 16:28:36 +0000

    added more unit tests

--- FrontEndv2.0/quasar-project/__tests__/AuthComponent.test.js
+++ FrontEndv2.0/quasar-project/__tests__/AuthComponent.test.js
@@ -8,5 +8,11 @@ describe('AuthComponent Test', () => {
   it('has a valid apiSecret', () => {
     expect(AuthComponent.data().apiSecret).toBe('qjFQ5WPxqJD7C0JZtMiORkzbhYAXjNNfX0WyMdx5GWz1IiZxFw')
   })
-
+  it('should redirect to /twitter when clicking the twitter button', () => {
+    const wrapper = mount(AuthComponent)
+    const button = wrapper.find("q-btn")
+    expect(button.exists()).toBe(true)
+    /*button.trigger('click')
+    expect(window.location.href).toBe('/twitter')*/
+  })
 })
