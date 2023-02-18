8f5762192a096eb1029ed0e830099fba3f2f9b37
duskxi <84715902+duskxi@users.noreply.github.com>
2023-02-18 16:59:56 +0000

    Docking web components

---
+++ service/web_components/Txt2Img.py
@@ -0,0 +1,51 @@
+import base64
+
+import cv2
+from flask import request
+
+from utility import TaskStatus
+from . import *
+
+
+class Txt2Img(Component):
+    def __init__(self, env: Environment):
+        super().__init__(env, '/api/v1/sample', 'Txt2Img', ['POST'])
+        self.env = env
+
+    def view(self):
+        data = request.get_json()
+        prompt = data['prompts']
+        negative_prompt = data['negative_prompt']
+        sampler = data['sample']
+        step = data['step']
+        width = data['width']
+        height = data['height']
+        n_iter = data['n_iter']
+        batch_size = data['batch_size']
+        cfg = data['cfg']
+        task_uuid = self.env.scheduler.add_task_txt2img(prompt, negative_prompt, sampler, step, width, height, n_iter, batch_size, cfg)
+        return {"status": 0, "uuid": task_uuid, "length": n_iter * batch_size}
+
+
+class Txt2ImgResult(Component):
+    def __init__(self, env: Environment):
+        super().__init__(env, '/api/v1/get_sample_result', 'Txt2Img', ['GET'])
+        self.env = env
+
+    def view(self):
+        uuid_str = request.args.get('uuid')
+        length = int(request.args.get('length'))
+        loaded = int(request.args.get('loaded'))
+        task = self.env.scheduler.get_task(uuid_str)
+        if task is None:
+            return {"status": 1}
+        if task.status == TaskStatus.Pending:
+            return {"status": 0, "images": []}
+        result = task.result
+        response = {"status": 0, "images": []}
+        for image in result[loaded:len(result)]:
+            retval, buffer = cv2.imencode('.jpg', cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
+            pic_str = base64.b64encode(buffer)
+            pic_str = pic_str.decode()
+            response['images'].append(pic_str)
+        return response
--- service/web_components/__init__.py
+++ service/web_components/__init__.py
@@ -1,1 +1,2 @@
+from utility import Task, Environment, Component
 from Index import Index
