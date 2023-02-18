6ddca06f560a6998c107044a9c48518b69e6816d
duskxi <84715902+duskxi@users.noreply.github.com>
2023-02-18 19:16:02 +0000

    server

--- service/web_server.py
+++ service/web_server.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from loguru import logger
@@ -5,12 +6,21 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 
+class NonASCIIJSONEncoder(json.JSONEncoder):
+    def __init__(self, **kwargs):
+        kwargs['ensure_ascii'] = False
+        super(NonASCIIJSONEncoder, self).__init__(**kwargs)
+
+
 class ApiServer:
     def __init__(self, static_url_path='', static_folder="quasar/public/dist/spa"):
         self.app = Flask(__name__,
                          static_url_path='',
                          static_folder=static_folder)
-        self.app.config['JSON_AS_ASCII'] = False
+        # self.app.config['JSON_AS_ASCII'] = False
+        self.app.config['RESTFUL_JSON'] = {
+            'ensure_ascii': False
+        }
         cors = CORS(self.app)
         self.app.config['CORS_HEADERS'] = 'Content-Type'
         log = logging.getLogger('werkzeug')
@@ -20,7 +30,7 @@ class ApiServer:
 
     def run(self, host='127.0.0.1', port=5000):
         logger.info(f"server is running on http://{host}:{port}")
-        self.app.run(threaded=True)
+        self.app.run(threaded=True, host=host, port=port)
 
     def register(self, path, func, methods=['GET']):
         func.__func__.__name__ += path
