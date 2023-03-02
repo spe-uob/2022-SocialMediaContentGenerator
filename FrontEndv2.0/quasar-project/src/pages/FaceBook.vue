5417a455661144c8b69f898ed3221ef5c9e03553
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-02 12:47:19 +0000

    Update FaceBook.vue

--- FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
+++ FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
@@ -1,11 +1,39 @@
 <template>
-
+ <div class="q-pa-md">
+  <div class="q-gutter-md" style="max-width: 300px">
+    <q-input rounded outlined v-model="text" label="Rounded outlined" />
+  </div>
+ </div>
 </template>
 
 <script>
+import axios from 'axios'
+import {ref} from 'vue'
 export default {
-  name: "FaceBook"
+  name: "FaceBook",
+
+
+  data() {
+    return {
+      message: ref(''),
+    };
+  },
+  methods: {
+    postToFacebook() {
+      axios.post('https://graph.facebook.com/me/feed', {
+        message: this.message,
+        access_token: 'your-access-token',
+      })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  },
 }
+
 </script>
 
 <style scoped>
