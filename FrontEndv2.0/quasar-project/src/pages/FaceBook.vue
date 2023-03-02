649791e6aafb4064e5597170faa76ab41399b5fc
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-02 13:52:52 +0000

    Update FaceBook.vue

--- FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
+++ FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="q-pa-md">
   <div class="q-gutter-md" style="max-width: 300px">
-    <q-input rounded outlined v-model="text" label="Rounded outlined" />
+    <q-input rounded outlined v-model="message" label="Rounded outlined" />
   </div>
  </div>
 </template>
