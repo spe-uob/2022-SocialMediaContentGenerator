9bc486e7a13868730535da2c82b3fc425e30d0cd
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-02 16:01:57 +0000

    Update FaceBookLogin.vue

--- FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
+++ FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex flex-center">
-      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="redirect" rounded></q-btn>
+      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="logInWithFacebook" rounded></q-btn>
       </div>
   </div>
 </template>
@@ -20,6 +20,40 @@ export default {
   methods: {
     redirect() {
       this.$router.push("/FaceBook")
+    },
+    async logInWithFacebook() {
+      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
+      await this.initFacebook();
+      window.FB.login(function(response) {
+        if (response.authResponse) {
+          alert("You are logged in &amp; cookie set!");
+          // Now you can redirect the user or do an AJAX request to
+          // a PHP script that grabs the signed request from the cookie.
+        } else {
+          alert("User cancelled login or did not fully authorize.");
+        }
+      });
+      return false;
+    },
+    async initFacebook() {
+      window.fbAsyncInit = function() {
+        window.FB.init({
+          appId: "8220179XXXXXXXXX", //You will need to change this
+          cookie: true, // This is important, it's not enabled by default
+          version: "v13.0"
+        });
+      };
+    },
+    async loadFacebookSDK(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
     }
   }
 }
