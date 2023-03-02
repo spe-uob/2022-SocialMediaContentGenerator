16fec9839fb80bc7a55952660f831fde24bee105
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-02 16:07:07 +0000

    Update FaceBook.vue

--- FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
+++ FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
@@ -24,41 +24,7 @@ export default {
       message: ''
     };
   },
-  methods: {
-    async logInWithFacebook() {
-      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
-      await this.initFacebook();
-      window.FB.login(function(response) {
-        if (response.authResponse) {
-          alert("login successfully");
-        } else {
-          alert("login failed");
-        }
-      });
-      this.$router.push("/twitter")
-      return false;
-    },
-    async initFacebook() {
-      window.fbAsyncInit = function() {
-        window.FB.init({
-          appId: "645161304039045",
-          cookie: true,
-          version: "v16.0"
-        });
-      };
-    },
-    async loadFacebookSDK(d, s, id) {
-      let js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_UK/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }
-  }
+  methods: {}
 }
 
 </script>
