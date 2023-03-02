eb67865497bd3940d5e1c135a038bf26913d99ec
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-02 14:19:52 +0000

    Update FaceBook.vue

--- FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
+++ FrontEndv2.0/quasar-project/src/pages/FaceBook.vue
@@ -1,7 +1,14 @@
 <template>
  <div class="q-pa-md">
   <div class="q-gutter-md" style="max-width: 1800px">
-    <q-input rounded outlined v-model="message" label="Text" />
+    <q-input rounded outlined v-model="message" label="Text" >
+      <template v-slot:append>
+        <q-icon name="close" @click="message = ''" class="cursor-pointer"/>
+      </template>
+      <template v-slot:after>
+        <q-icon name="send" @click="postToFacebook" color="primary" class="cursor-pointer" :disable="!message"/>
+      </template>
+      </q-input>
   </div>
  </div>
 </template>
@@ -22,7 +29,7 @@ export default {
     postToFacebook() {
       axios.post('https://graph.facebook.com/me/feed', {
         message: this.message,
-        access_token: 'your-access-token',
+        access_token: 'EAAJKxVJZAnoUBANeAl5XZCZCp0BN0PMAkUZABYzodsOXZCKY10Eqy5czitMf0gzp7ouMBcO3ZCCw938pkF3lnVEjSGT42T6LIibuZAauMZBNrx2IS6WTXM2vPll17y04X5uh0bF4iVqQp4qaRE1pm9fg90WrRsE6axycks5lyI2NJ2dDRnWa5qLxCkboTBUrFmfEWKxBlYS8zm4nutQqrjWelD1WVWJTHxMZD',
       })
         .then(response => {
           console.log(response);
