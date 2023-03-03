5f475ee188b56c5d471e56c0c36dc4b902f71440
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-03 09:25:54 +0000

    Update FaceBookLogin.vue

--- FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
+++ FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
@@ -6,64 +6,35 @@
   </div>
 </template>
 
-import axios from 'axios'
 
 <script>
+import {getFBLoginStatus, loadFBSDK} from "boot/FB";
 export default {
   name: "FaceBookLogin",
 
   data() {
     return {
+      isConnected:false,
       message: '',
     };
+  },
+  mounted() {
+    loadFBSDK()
+      .then(getFBLoginStatus)
+      .then(response => {
+        if (response.status === 'connected') {
+          this.isConnected = true
+        }
+      })
   },
   methods: {
     redirect() {
       this.$router.push("/FaceBook")
     },
-    async logInWithFacebook() {
-      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
-      await this.initFacebook();
-      window.FB.login(function (response) {
-        if (response.authResponse) {
-          alert("login successfully");
-        } else {
-          alert("login failed");
-        }
-      });
-      this.$router.push("/FaceBook")
-      return false;
-    },
-    async initFacebook() {
-      window.fbAsyncInit = function () {
-        window.FB.init({
-          appId: "645161304039045",
-          cookie: true,
-          version: "v16.0"
-        });
-      };
-    },
-    async loadFacebookSDK() {
-      return new Promise(resolve => {
-        window.fbAsyncInit = function () { // eslint-disable-line func-names
-          FB.init({
-            appId,
-            xfbml: false,
-            version,
-            cookie: true
-          });
-          FB.AppEvents.logPageView();
-          resolve('SDK Loaded!');
-        };
-        (function (d, s, id) { // eslint-disable-line func-names
-          const fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) { return; }
-          const js = d.createElement(s); js.id = id;
-          js.src = '//connect.facebook.net/en_US/sdk.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      });
+    logInWithFacebook(){
+      this.redirect()
     }
+
   }
 }
 </script>
