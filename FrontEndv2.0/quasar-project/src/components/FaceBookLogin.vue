61100ed3a0760c2a66e0925a394b2e6dcbfc2733
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-03 10:54:11 +0000

    Update FaceBookLogin.vue

--- FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
+++ FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <div class="flex flex-center">
-      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="logInWithFacebook" rounded></q-btn>
+      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="FaceBook" rounded></q-btn>
       </div>
   </div>
 </template>
 
 
 <script>
-import {getFBLoginStatus, loadFBSDK} from "boot/FB";
+import {getAuth,  FacebookAuthProvider, signInWithPopup} from "firebase/auth"
+const provider = new FacebookAuthProvider();
 export default {
   name: "FaceBookLogin",
 
@@ -18,23 +19,25 @@ export default {
       message: '',
     };
   },
-  mounted() {
-    loadFBSDK()
-      .then(getFBLoginStatus)
-      .then(response => {
-        if (response.status === 'connected') {
-          this.isConnected = true
-        }
-      })
-  },
-  methods: {
-    redirect() {
-      this.$router.push("/FaceBook")
-    },
-    logInWithFacebook(){
-      this.redirect()
-    }
 
+  methods: {
+    async FaceBook(){
+      const auth = getAuth();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const user = result.user;
+          const credential = FacebookAuthProvider.credentialFromResult(result);
+          const accessToken = credential.accessToken;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          const email = error.customData.email;
+          const credential = FacebookAuthProvider.credentialFromError(error);
+        },
+      this.$router.push("/FaceBook"));
+
+    },
   }
 }
 </script>
