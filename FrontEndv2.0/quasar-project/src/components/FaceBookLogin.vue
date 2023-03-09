1d4859afef049b185bd4c3b6f08584d4682af0e0
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-09 12:44:36 +0000

    Update FaceBookLogin.vue

--- FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
+++ FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex flex-center">
-      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="FaceBook" rounded></q-btn>
+      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="FB_firebase" rounded></q-btn>
       </div>
   </div>
 </template>
@@ -9,19 +9,22 @@
 
 <script>
 import {getAuth,  FacebookAuthProvider, signInWithPopup} from "firebase/auth"
+import {FBLogin, getFBLoginStatus, loadFBSDK} from "boot/FB"
 const provider = new FacebookAuthProvider();
 export default {
   name: "FaceBookLogin",
 
   data() {
     return {
+      appID:"645161304039045",
+      version:"v16.0",
       isConnected:false,
       message: '',
     };
   },
 
   methods: {
-    async FaceBook(){
+    async FB_firebase(){
       const auth = getAuth();
       signInWithPopup(auth, provider)
         .then((result) => {
@@ -38,6 +41,39 @@ export default {
       this.$router.push("/FaceBook"));
 
     },
+    FB_fbsdk(){
+      FBLogin(this.loginOptions)
+        .then(response => {
+          if (response.status === 'connected') {
+            this.isConnected = true;
+          } else {
+            this.isConnected = false;
+          }
+          this.$emit('login', {
+            response,
+            FB: window.FB
+          })
+        });
+
+
+    },
+
+    mounted() {
+      loadFBSDK(this.appId, this.version)
+        .then(loadingResult => {
+        })
+        .then(() => getLoginStatus())
+        .then(response => {
+          if (response.status === 'connected') {
+            this.isConnected = true;
+          }
+          this.$emit('get-initial-status', response);
+          this.$emit('sdk-loaded', {
+            isConnected: this.isConnected,
+            FB: window.FB
+          })
+        });
+    }
   }
 }
 </script>
