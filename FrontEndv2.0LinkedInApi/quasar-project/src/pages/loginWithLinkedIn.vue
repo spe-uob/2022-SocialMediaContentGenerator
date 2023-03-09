cef10748bf533591664b6decb6ef1a95f24dc8b4
Flgodd <benjyaram@gmail.com>
2023-03-09 16:10:04 +0000

    api attempt

--- FrontEndv2.0LinkedInApi/quasar-project/src/pages/loginWithLinkedIn.vue
+++ FrontEndv2.0LinkedInApi/quasar-project/src/pages/loginWithLinkedIn.vue
@@ -1,48 +1,52 @@
 <template>
-  <q-btn label="Login with LinkedIn" @click="loginWithLinkedIn" />
+  <q-btn label="Login with LinkedIn" @click="authorize" />
 </template>
 
 <script>
-import axios from 'axios';
+import { linkedinAuthUrl, getAccessToken } from '../boot/linkedin.js'
 export default {
+
   name: "loginWithLinkedIn",
   methods: {
-    async loginWithLinkedIn() {
-      try {
-        const response = await axios.post('https://www.linkedin.com/oauth/v2/accessToken', {
-          grant_type: 'authorization_code',
-          code: 'AUTHORIZATION_CODE',
-          redirect_uri: 'REDIRECT_URI',
-          client_id: 'CLIENT_ID',
-          client_secret: 'CLIENT_SECRET'
-        });
-
-       // const accessToken = response.data.access_token;
-        const accessToken ='AQWhwY2PZI0VfUx-N_q9_zmV4QExn0X7UKOd1gXSKEZeunjyVtRMhNyipliSAuLHIcyeexoNvh3WwuG2WTH1fCtJ9ndMnE2meViNNa56mma5G-jfH4s1WKqEargNUNN205Lw65w5wfdidR2k3kDH8eY9-dK3snyM4Ki_RB9hRCBuwMAbLeeDhQDOHU46RFV4PUn6_7V9hWO35oN43Z7j1hADJyOE3aLrcbKHohTH9z5__ALvTvlkw2X4l8gYJA9K6cJyd4oGXZIJ3cFkGJZgz7ij31inj5HoULfrVZ68H7T3OWywPkBinewQgtohhtzTpmsFFY1njDZ6JcUEWT4A74wHDYicTA';
-
-
-        // Use the access token to make API requests and retrieve user data
-        await this.retrieveUserData(accessToken);
-      } catch (error) {
-        console.error(error);
-      }
+    authorize() {
+      // Redirect the user to the LinkedIn authorization page
+      window.location.href = linkedinAuthUrl
     },
-
-    async retrieveUserData(accessToken) {
-      try {
-        const response = await axios.get('https://api.linkedin.com/v2/me', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`
-          }
-        });
-
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
+    retrieveAccessToken() {
+      const code = new URLSearchParams(window.location.search).get('code')
+      if (code) {
+        getAccessToken(code)
+          .then(access_token => {
+            // Use the access token to make API requests
+            axios.get('https://api.linkedin.com/v2/me', {
+              headers: {
+                'Authorization': `Bearer ${access_token}`,
+                'cache-control': 'no-cache',
+                'X-Restli-Protocol-Version': '2.0.0',
+              }
+            })
+              .then(response => {
+                // Handle the API response
+                console.log(response.data)
+              })
+              .catch(error => {
+                // Handle the API error
+                console.error(error)
+              })
+          })
+          .catch(error => {
+            // Handle the access token retrieval error
+            console.error(error)
+          })
       }
     }
+  },
+  mounted() {
+    this.retrieveAccessToken()
   }
 }
+
+
 </script>
 
 <style scoped>
