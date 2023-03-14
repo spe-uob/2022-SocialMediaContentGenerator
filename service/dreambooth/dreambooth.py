89a3c6eac147975a3fc501e516fe3aeff72e25ff
duskxi <84715902+duskxi@users.noreply.github.com>
2023-03-14 18:51:28 +0000

    load concept

--- service/dreambooth/dreambooth.py
+++ service/dreambooth/dreambooth.py
@@ -1,3 +1,4 @@
+from service.dreambooth.concept import ConceptLoader
 from service.dreambooth.dreambooth_config import DreamboothConfig
 
 
@@ -7,5 +8,6 @@ class Dreambooth:
             self.config = DreamboothConfig(**kwargs)
         else:
             self.config = config
-
+        self.concept_loader = ConceptLoader(concept_loader_args)
+        self.concepts = self.concept_loader.concepts
 
