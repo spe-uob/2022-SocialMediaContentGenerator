93f128a3b28c3baa5d0f1f9e3c3cc2b1bcd93813
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-19 10:47:46 +0000

    Update firebase.js

--- fbapi_fetch/quasar-project/src/boot/firebase.js
+++ fbapi_fetch/quasar-project/src/boot/firebase.js
@@ -13,9 +13,9 @@ firebase.initializeApp(firebaseConfig)
 firebase.getCurrentUser = () => {
   return new Promise((resolve, reject) => {
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      unsubscribe();
-      resolve(user);
-    }, reject);
+      unsubscribe()
+      resolve(user)
+    }, reject)
   })
 }
 export default firebase
