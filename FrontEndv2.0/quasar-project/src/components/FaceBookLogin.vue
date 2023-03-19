f2277c4af4c10f594a20aeb3d4555d6bff9cead1
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-19 16:06:13 +0000

    Delete the component hook

--- FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
+++ FrontEndv2.0/quasar-project/src/components/FaceBookLogin.vue
@@ -44,25 +44,6 @@ export default {
         })
       this.fb = fb
     },
-    created () {
-      const firebaseConfig = {
-        apiKey: "AIzaSyAhLly_xBcWA2FTfeKe726EGCJmQD7mGHo",
-        authDomain: "socialmediacontentgenerator.firebaseapp.com",
-        projectId: "socialmediacontentgenerator",
-        storageBucket: "socialmediacontentgenerator.appspot.com",
-        messagingSenderId: "205591394295",
-        appId: "1:205591394295:web:474e8be42ce7a64edde0f2"
-      }
-      firebase.initializeApp(firebaseConfig)
-      firebase.getCurrentUser = () => {
-        return new Promise((resolve, reject) => {
-          const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-            unsubscribe()
-            resolve(user)
-          }, reject)
-        })
-      }
-    }
   }
 }
 </script>
