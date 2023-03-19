1563155e6dda3ef3666d8eb378f82aeeabdc5b38
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-19 12:30:41 +0000

    Create FaceBook.vue

---
+++ fbapi_fetch/quasar-project/src/components/FaceBook.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex flex-center">
+      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="loginWithFacebook" rounded></q-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getAuth, signInWithPopup, FacebookAuthProvider } from 'firebase/auth'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'FaceBook',
+  methods: {
+    async loginWithFacebook () {
+      const provider = new FacebookAuthProvider()
+      try {
+        const auth = getAuth()
+        const result = await signInWithPopup(auth, provider)
+        const user = result.user
+        const credential = provider.credentialFromResult(result)
+        this.$emit('login', user)
+        this.$emit('access', credential)
+      } catch (error) {
+        // Handle error
+      }
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
