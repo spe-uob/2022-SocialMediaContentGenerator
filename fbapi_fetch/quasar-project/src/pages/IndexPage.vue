ea1c8808bfab4f26441923f87e7098acfc217c78
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-19 12:30:45 +0000

    Update IndexPage.vue

--- fbapi_fetch/quasar-project/src/pages/IndexPage.vue
+++ fbapi_fetch/quasar-project/src/pages/IndexPage.vue
@@ -6,7 +6,10 @@
       style="width: 200px; height: 200px"
     >
     <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="login" rounded></q-btn>
-    <v-facebook-login app-id="645161304039045"></v-facebook-login>
+    <v-facebook-login app-id="645161304039045">
+      <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit"  rounded></q-btn>
+    </v-facebook-login>
+    <FaceBook @login="handleFacebookLogin"></FaceBook>
 
   </q-page>
 </template>
@@ -14,10 +17,11 @@
 <script>
 import VFacebookLogin from 'vue-facebook-login-component-next'
 import { defineComponent } from 'vue'
-import {} from 'boot/FB'
+import FaceBook from 'components/FaceBook'
 
 export default defineComponent({
   components: {
+    FaceBook,
     VFacebookLogin
   },
   name: 'IndexPage',
@@ -31,10 +35,8 @@ export default defineComponent({
     }
   },
   methods: {
-    async login () {
-
-    },
-    created () {
+    handleFacebookLogin (user) {
+      this.displayName = user.displayName
     }
   }
 })
