67bf624e8775553dae3cf838f2baf740caefc3da
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-19 10:47:51 +0000

    Update IndexPage.vue

--- fbapi_fetch/quasar-project/src/pages/IndexPage.vue
+++ fbapi_fetch/quasar-project/src/pages/IndexPage.vue
@@ -5,18 +5,36 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="Login" rounded></q-btn>
+    <q-btn class="text-center full-width" color="blue" icon="fa-brands fa-facebook" label="sign in with facebook" type="submit" @click="login" rounded></q-btn>
+    <v-facebook-login app-id="645161304039045"></v-facebook-login>
 
   </q-page>
 </template>
 
 <script>
+import VFacebookLogin from 'vue-facebook-login-component-next'
 import { defineComponent } from 'vue'
+import {} from 'boot/FB'
 
 export default defineComponent({
+  components: {
+    VFacebookLogin
+  },
   name: 'IndexPage',
+  data () {
+    return {
+      fb: '',
+      text: '',
+      image: '',
+      accessToken: '',
+      displayName: ''
+    }
+  },
   methods: {
-    login () {
+    async login () {
+
+    },
+    created () {
     }
   }
 })
