6ef1d33904a0f544a7ae39dabfcbbbf97bbf1015
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-20 22:38:21 +0000

    enable image publish

--- FrontEndv2.0/quasar-project/src/pages/FaceBookPost.vue
+++ FrontEndv2.0/quasar-project/src/pages/FaceBookPost.vue
@@ -21,34 +21,86 @@ export default {
   data(){
     return{
       message:"",
-      picture:"~src/quasar-logo-vertical.svg",
+      image:"",
+      imageUrl:"https://images.app.goo.gl/Pezq2abNj8EP7SKr8",
       user_token:"",
       page_token:""
     }
   },
   methods: {
-    postToFacebook(){
+    async postToFacebook() {
       console.log("user access token:", this.$route.query.user_token)
       console.log("page access token:", this.$route.query.page_token)
       this.page_token = this.$route.query.page_token
       this.user_token = this.$route.query.user_token
-      const formData = new FormData();
-      formData.append('message', 'Hello, World!');
-      formData.append('source', '~assets/quasar-logo-vertical.svg');
+      const image_path = "src/assets/quasar-logo-vertical.svg"
+      const caption = this.message
+      //formData.append('source', this.image);
       console.log("message sent", this.message)
       console.log("page token", this.page_token)
-      axios.post(`https://graph.facebook.com/${page_id}/feed`, {
-        message: this.message,
-        access_token: this.page_token,
-        picture:this.picture
+      fetch('src/assets/spaceNXT.png')
+        .then(response => response.blob())
+        .then(blob => {
+
+          // Create a FormData object and append the image blob to it
+          const formData = new FormData();
+          formData.append('source', blob, 'spaceNXT.png');
+
+          // Make the POST request to Facebook Graph API
+          axios.post('https://graph.facebook.com/119057271096466/photos', formData, {
+            params: {
+              access_token: this.page_token,
+              caption: this.message
+            },
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          })
+            .then(response => {
+              console.log(response.data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+
+
+
+
+      /*axios.post(`https://graph.facebook.com/${page_id}/photos`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });*/
+      /*const result = await fetch(`https://graph.facebook.com/${page_id}/photos`, {
+        method: 'POST',
+        body: formData
+      }).then(response => response.json())
+      console.log(result)*/
+
+     /* axios.post(`https://graph.facebook.com/${page_id}/photos`, {
+        //message: this.message,
+        url: "https://unsplash.com/photos/tJxu4j4-T4o",
+        caption: this.message,
+        access_token: this.page_token
       })
         .then(response => {
           console.log('Response:', response);
         })
         .catch(error => {
           console.error('Error:', error);
-        });
-    }
+        });*/
+    },
   }
 }
 </script>
