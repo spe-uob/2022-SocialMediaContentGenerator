4795e0c06b25b4493b9c090018c40b42a40a9b77
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-03-20 15:17:41 +0000

    enable data passing between pages

--- FrontEndv2.0/quasar-project/src/components/FBAuthComponent.vue
+++ FrontEndv2.0/quasar-project/src/components/FBAuthComponent.vue
@@ -22,32 +22,44 @@ export default {
       appID:"645161304039045",
       version:"v16.0",
       user_token:'',
-      page_token:''
+      page_token:'',
+      user_id:''
     };
   },
 
   methods: {
     async login () {
       const auth = getAuth()
-      const pageId = '101969492843962'
+      const pageId = '119057271096466'
       let fb = await signInWithPopup(auth, provider)
         .then((result) => {
           const credential = FacebookAuthProvider.credentialFromResult(result)
           const token = credential.accessToken
           const user = result.user
-          console.log(token)
+          this.user_id = user.uid
+          console.log(user)
+          console.log("User access token:",token)
           this.user_token = token
+          console.log("User access token:", this.user_token)
           fetch(`https://graph.facebook.com/${pageId}?fields=access_token&access_token=${token}`)
             .then(response => response.json())
             .then(data => {
               const pageAccessToken = data.access_token
               console.log('Page access token:', pageAccessToken)
               this.page_token = pageAccessToken
+              console.log('Page access token:', this.page_token)
+              this.$router.push({
+                path: '/FaceBookPost',
+                query: {
+                  user_id: this.user_id,
+                  user_token: this.user_token,
+                  page_token: this.page_token
+                }
+              })
             })
             .catch(error => {
               console.error('Error:', error);
             });
-          this.$router.push("/FaceBookPost")
         }).catch((error) => {
           console.log(error.code)
           console.log(error.message)
