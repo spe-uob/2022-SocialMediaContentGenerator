0a29099b23fe7fc9ba78a5af1baf4004fc6c3e54
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-24 23:01:11 +0100

    Update Facebook.py

--- service/web_components/Facebook.py
+++ service/web_components/Facebook.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import json
-
+import facebook
 class FacebookBackEnd(Component):
     def __init__(self, env:Environment):
         super.__init__(env, 'api/v1/facebook', FacebookBackEnd, ['POST'])
@@ -8,10 +8,16 @@ class FacebookBackEnd(Component):
 
     def view(self):
         data = request.get_json()
-
+        message = data['message']
+        image = data['image']
         with open('facebook_auth.json') as json_file:
             auth_dict = json.load(json_file)
             userAccessToken = auth_dict['userAccessToken']
             pageAccessToken = auth_dict['pageAccessToken']
             pageID = auth_dict['pageID']
+        graph = facebook.GraphAPI(access_token = userAccessToken, version='16.0')
+        photo_id = graph.put_photo(image=image)
+        graph.put_object(parent_object=pageID, connection_name='feed', message=message,
+                         picture='https://www.facebook.com/photo.php?fbid=' + photo_id)
+
 
