1ab795de5b064c020ef58921f003c8ad86d0aca3
Flgodd <benjyaram@gmail.com>
2023-04-24 15:55:09 +0100

    changes

--- web/src/pages/LinkedInPost.vue
+++ web/src/pages/LinkedInPost.vue
@@ -23,18 +23,14 @@
 <script>
 
 import axios from 'axios';
-//https://www.linkedin.com/oauth/v2/authorization?client_id=78sme225fsy5by&redirect_uri=http://localhost:9000&response_type=code&scope=r_emailaddress r_liteprofile w_member_social
 
-
-// https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&scope=${scope}
-// https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile%20r_emailaddress&client_id=78sme225fsy5by&redirect_uri=http%3A%2F%2Flocalhost%3A9000
 
 const client_id =  '78sme225fsy5by'
 const redirect_uri = 'http://localhost:9000'
 const scope =  'r_liteprofile r_emailaddress w_member_social'
 const state = Math.random().toString(36).substring(7)
 const linkedinAuthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&scope=${scope}`
-//`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78sme225fsy5by&redirect_uri=http%3A%2F%2Flocalhost%3A9000&state=987654321&scope=r_liteprofile_r_emailaddress_w_member_social`
+
 
 
 export default {
