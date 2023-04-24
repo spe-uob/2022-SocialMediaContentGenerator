13fcb78cb4baf3c03a4ae77bfe54282fbdef25ab
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-24 21:50:00 +0100

    Update Facebook.py

--- service/web_components/Facebook.py
+++ service/web_components/Facebook.py
@@ -4,6 +4,3 @@ class FacebookBackEnd(Component):
     def __init__(self, env:Environment):
         super.__init__(env, 'api/v1/facebook', FacebookBackEnd, ['POST'])
         self.env = env
-
-    def view(self):
-        data = request.get_json()
