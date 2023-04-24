e3e867ff57a50417df8c19ac8b24209553fe759d
Flgodd <benjyaram@gmail.com>
2023-04-24 15:52:03 +0100

    linkedin api front end

--- web/src/pages/LinkedInPost.vue
+++ web/src/pages/LinkedInPost.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile%20r_emailaddress&client_id=78sme225fsy5by&redirect_uri=http%3A%2F%2Flocalhost%3A9000">
+    <a href="https://www.linkedin.com/oauth/v2/authorization?client_id=78sme225fsy5by&redirect_uri=http://localhost:9000&response_type=code&scope=r_emailaddress r_liteprofile w_member_social" @click="getCode">
       <img src="/path/to/linkedin-icon.png" alt="Log in with LinkedIn" />
     </a>
   </div>
@@ -10,10 +10,10 @@
         <h2 class="q-ma-none">Publish a post on LinkedIn</h2>
       </q-card-section>
       <q-card-section>
-        <q-form @submit="publishPost">
-          <q-input v-model="post.subject" label="Post subject"></q-input>
-          <q-input v-model="post.text.text" label="Post text" type="textarea"></q-input>
-          <q-btn type="submit" label="Publish post"></q-btn>
+        <q-form @submit="submitPost">
+          <q-input label="Post subject"></q-input>
+          <q-input label="Post text" v-model="inputText" type="textarea"></q-input>
+          <q-btn type="submit" label="Publish post xxx"></q-btn>
         </q-form>
       </q-card-section>
     </q-card>
@@ -21,42 +21,50 @@
 </template>
 
 <script>
+
 import axios from 'axios';
+//https://www.linkedin.com/oauth/v2/authorization?client_id=78sme225fsy5by&redirect_uri=http://localhost:9000&response_type=code&scope=r_emailaddress r_liteprofile w_member_social
+
+
+// https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&scope=${scope}
+// https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile%20r_emailaddress&client_id=78sme225fsy5by&redirect_uri=http%3A%2F%2Flocalhost%3A9000
+
+const client_id =  '78sme225fsy5by'
+const redirect_uri = 'http://localhost:9000'
+const scope =  'r_liteprofile r_emailaddress w_member_social'
+const state = Math.random().toString(36).substring(7)
+const linkedinAuthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&scope=${scope}`
+//`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78sme225fsy5by&redirect_uri=http%3A%2F%2Flocalhost%3A9000&state=987654321&scope=r_liteprofile_r_emailaddress_w_member_social`
+
+
 export default {
-  name: "MyLinkedInPost",
+  name: "LinkedInPost",
   data () {
     return {
-      post: {
-        subject: '',
-        text: {
-          text: ''
-        }
-      },
-      accessToken: 'AQWhwY2PZI0VfUx-N_q9_zmV4QExn0X7UKOd1gXSKEZeunjyVtRMhNyipliSAuLHIcyeexoNvh3WwuG2WTH1fCtJ9ndMnE2meViNNa56mma5G-jfH4s1WKqEargNUNN205Lw65w5wfdidR2k3kDH8eY9-dK3snyM4Ki_RB9hRCBuwMAbLeeDhQDOHU46RFV4PUn6_7V9hWO35oN43Z7j1hADJyOE3aLrcbKHohTH9z5__ALvTvlkw2X4l8gYJA9K6cJyd4oGXZIJ3cFkGJZgz7ij31inj5HoULfrVZ68H7T3OWywPkBinewQgtohhtzTpmsFFY1njDZ6JcUEWT4A74wHDYicTA'
+      url: linkedinAuthUrl,
+      inputText: ''
     }
   },
   methods: {
-    publishPost () {
-      const shareEndpoint = 'https://api.linkedin.com/v2/shares';
-      const postData = {
-        owner: 'urn:li:person:USER_ID',
-        subject: this.post.subject,
-        text: {
-          text: this.post.text.text
-        }
-        // Add any other relevant parameters here, such as visibility settings or image attachments
-      };
-      axios.post(shareEndpoint, postData, {
-        headers: {
-          Authorization: `Bearer ${this.accessToken}`
-        }
-      }).then(response => {
-        console.log('Post published:', response.data);
-      }).catch(error => {
-        console.error('Failed to publish post:', error.response.data);
-      });
+    getCode(){
+      const code = new URL(location.href).searchParams.get('code')
+      console.log(code)
+      if(code){
+        axios.post("http://localhost:8888/api/linkedin/access_token", {code: code})
+      }
+    },
+    submitPost(){
+      let message = this.inputText
+      console.log("submit pressed")
+      if(message){
+        axios.post("http://localhost:8888/api/linkedin/post", {message: message})
+      }
     }
+  },
+  mounted() {
+    this.getCode();
   }
+
 }
 </script>
 
