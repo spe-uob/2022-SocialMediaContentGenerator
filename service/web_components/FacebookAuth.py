3270e7ac55d6ee6086818b720b03ca28ea1d06ec
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-24 21:50:03 +0100

    Update FacebookAuth.py

--- service/web_components/FacebookAuth.py
+++ service/web_components/FacebookAuth.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
-class TwitterAuth(Component):
+import json
+
+
+class FacebookAuth(Component):
     def __init__(self, env: Environment):
-        super().__init__(env, '/api/v1/twitterAuth', TwitterAuth, ['POST'])
+        super().__init__(env, '/api/v1/twitterAuth', FacebookAuth, ['POST'])
         self.env = env
 
     def view(self):
         data = request.get_json()
         userAccessToken = data['userAccessToken']
-        access_token_secret = data['access_token_secret']
-
+        pageAccessToken = data['pageAccessToken']
+        pageID = data['pageID']
         auth_dict = {
-        'access_token': access_token,
-        'access_token_secret': access_token_secret,
-        'consumer_key': "EzoH0w73hC3naY84U6NBHZHyz",
-        'consumer_secret': "qjFQ5WPxqJD7C0JZtMiORkzbhYAXjNNfX0WyMdx5GWz1IiZxFw"
+            'userAccessToken': userAccessToken,
+            'pageAccessToken': pageAccessToken,
+            'pageId': pageID
         }
 
-        json_object = json.dumps(auth_dict, indent=4)
+        json_object = json.dumps(auth_dict, indent=3)
 
-        with open('twitter_auth.json', 'w') as outfile:
-         outfile.write(json_object)
+        with open('facebook_auth.json', 'w') as outfile:
+            outfile.write(json_object)
 
         return {'status': 'ok', 'auth': auth_dict}
