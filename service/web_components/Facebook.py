dba9abab25f04c59da32988f25d9600e0453c7d2
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-24 22:01:55 +0100

    update facebook back-end

--- service/web_components/Facebook.py
+++ service/web_components/Facebook.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, jsonify
+import json
 
 class FacebookBackEnd(Component):
     def __init__(self, env:Environment):
         super.__init__(env, 'api/v1/facebook', FacebookBackEnd, ['POST'])
         self.env = env
+
+    def view(self):
+        data = request.get_json()
+
+        with open('facebook_auth.json') as json_file:
+            auth_dict = json.load(json_file)
+            userAccessToken = auth_dict['userAccessToken']
+            pageAccessToken = auth_dict['pageAccessToken']
+            pageID = auth_dict['pageID']
+
