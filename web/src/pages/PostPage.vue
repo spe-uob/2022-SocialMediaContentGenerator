ae6f40f6e006bee1bedb9e7b6a08059082303d2c
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-25 15:18:24 +0100

    Update PostPage.vue

--- web/src/pages/PostPage.vue
+++ web/src/pages/PostPage.vue
@@ -1,10 +1,19 @@
 <template>
-
+  <q-img
+    :src=this.image
+    spinner-color="white"
+    style="height: 140px; max-width: 150px"
+  />
 </template>
 
 <script>
 export default {
-  name: "PostPage"
+  name: "PostPage",
+  data() {
+    return{
+      image:""
+    }
+}
 }
 </script>
 
