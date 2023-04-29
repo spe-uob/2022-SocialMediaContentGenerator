8f1f81f453fb16a23c62e59f325634e3c9958a5e
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-29 20:19:57 +0100

    Update LinkedIn.py

--- service/SMCGlibrary/LinkedIn.py
+++ service/SMCGlibrary/LinkedIn.py
@@ -10,53 +10,41 @@ class LinkedIn:
         with open("linkedin_auth.json") as json_file:
             auth_dict = json.load(json_file)
             access_token = auth_dict["access_token"]
+            APPLICATION_KEY = auth_dict["APPLICATION_KEY"]
+            APPLICATION_SECRET = auth_dict["APPLICATION_SECRET"]
+            profile = auth_dict["profile"]
         self.access_token = access_token
+        self.APPLICATION_KEY = APPLICATION_KEY
+        self.APPLICATION_SECRET = APPLICATION_SECRET
+        self.profile = profile
 
-    def post(self, text):
-        image = request.files['image']
-        url = 'https://api.linkedin.com/rest/images?action=initializeUpload'
+    def post(self, text, image):
         application = linkedin.LinkedInApplication(token=self.access_token)
-        response = application.make_request('GET', 'https://api.linkedin.com/v2/me')
-        profile = response.text
-        profile_d = json.loads(profile)
-        user_id = profile_d['id']
-        message = {
-            "initializeUploadRequest": {
-                "owner": f"urn:li:person:{user_id}"
+        image_url = image
+        image_data = application.submit_image(image_url=image_url)
+        text = text
+        post_data = {
+            'comment': text,
+            'content': {
+                'submitted-url': 'https://example.com/',
+                'submitted-image-url': image_data['id']
+            },
+            'visibility': {
+                'code': 'anyone'
             }
         }
-        payload = json.dumps(message)
-        response = request.post(url,
-                                headers={'Authorization': 'Bearer ' + self.access_token,
-                                         'Content-Type': 'application/json',
-                                         'LinkedIn-Version': '202304', 'X-Restli-Protocol-Version': '2.0.0'},
-                                data=payload)
-        res = response.json()
-        uploadUrl = res['value']['uploadUrl']
-        image_id = res['value']['image']
-        responseImage = request.put(uploadUrl, headers={'X-Restli-Protocol-Version': '2.0.0',
-                                                        'Authorization': 'Bearer ' + self.access_token,
-                                                        'LinkedIn-Version': '202304'}, data=image)
-        responseImage.raise_for_status()
-        post_d = {
-            "author": f"urn:li:person:{user_id}",
-            "commentary": "Sample video Post",
-            "visibility": "PUBLIC",
-            "distribution": {
-                "feedDistribution": "MAIN_FEED",
-                "targetEntities": [],
-                "thirdPartyDistributionChannels": []
-            },
-            "content": {
-                "media": {
-                    "title": "title of the video",
-                    "id": image_id
-                }
-            },
-            "lifecycleState": "PUBLISHED",
-        }
+        response = application.submit_share(**post_data)
 
-        payload = json.dumps(post_d)
-        r = request.post("https://api.linkedin.com/rest/posts",
-                          headers={'Authorization': 'Bearer ' + self.access_token, 'Content-Type': 'application/json',
-                                   'X-Restli-Protocol-Version': '2.0.0', 'LinkedIn-Version': '202304'}, data=payload)
+    def check(self):
+        try:
+            status = "signedIn"
+            name = self.profile['firstName'] + " " + self.profile['lastName']
+        except linkedin.LinkedInError as e:
+            status = "notSignedIn"
+            name = ""
+        return {'status': status, 'name': name}
+
+
+
+
+
