b7e1af70a6ef02f23b38d5703eb4dbad788f7be0
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-29 17:40:22 +0100

    Add twitterAuth

---
+++ service/web_components/LoginAPI.py
@@ -0,0 +1,41 @@
+from . import *
+from flask import Flask, request, jsonify
+from linkedin import linkedin
+from linkedin.linkedin import PERMISSIONS
+import json
+import tweepy
+from service.SMCGlibrary.Twitter import Twitter
+from service.SMCGlibrary.Facebook import Facebook
+from service.SMCGlibrary.LinkedIn import LinkedIn
+
+
+class LoginAPI(Component):
+    def __init__(self, env: Environment):
+        super().__init__(env, '/api/v1/Login', LoginAPI, ['POST'])
+        self.env = env
+
+    def view(self):
+        data = request.get_json()
+        if data['platform'] == "twitter":
+            self.twitterAuth(data)
+        elif data['platform'] == "facebook":
+            self.facebookAuth(data)
+        elif data['platform'] == "linkedin":
+            self.linkedinAuth(data)
+
+    def twitterAuth(self, data):
+        access_token = data['access_token']
+        access_token_secret = data['access_token_secret']
+        auth_dict = {
+            'access_token': access_token,
+            'access_token_secret': access_token_secret,
+            'consumer_key': "EzoH0w73hC3naY84U6NBHZHyz",
+            'consumer_secret': "qjFQ5WPxqJD7C0JZtMiORkzbhYAXjNNfX0WyMdx5GWz1IiZxFw"
+        }
+        json_object = json.dumps(auth_dict, indent=4)
+        with open('twitter_auth.json', 'w') as outfile:
+            outfile.write(json_object)
+
+    
+
+
