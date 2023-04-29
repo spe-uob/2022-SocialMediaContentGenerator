a88661695bdd26321f7d12ebc655525190c7e7c9
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-29 16:56:22 +0100

    init TwitterAPI.py

--- service/web_components/TwitterApi.py
+++ service/web_components/TwitterApi.py
@@ -1,5 +1,9 @@
 from . import *
 from flask import request
-import tweepy
-import io
 import json
+import tweepy
+
+class TwitterAPI (Component):
+    def __init__(self, env:Environment):
+        super().__init__(env, '/api/v1/Twitter', TwitterAPI, ['POST'])
+        self.env = env
