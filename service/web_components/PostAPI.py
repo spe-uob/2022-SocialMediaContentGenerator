df6bac4185a5b01e67c5643adde9fbc890dae8d5
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-29 17:59:02 +0100

    Init PostAPI.py

--- service/web_components/PostAPI.py
+++ service/web_components/PostAPI.py
@@ -7,3 +7,22 @@ import tweepy
 from service.SMCGlibrary.Twitter import Twitter
 from service.SMCGlibrary.Facebook import Facebook
 from service.SMCGlibrary.LinkedIn import LinkedIn
+
+class PostAPI(Component):
+    def __init__(self,env:Environment):
+        super().__init__(env,'/api/v1/Post',PostAPI,['POST'])
+        self.env = env
+    def view(self):
+        data = request.get_json()
+        if data['platform'] == "twitter":
+            self.twitterPost(data)
+        elif data['platform'] == "facebook":
+            self.facebookPost(data)
+        elif data['platform'] == "linkedin":
+            self.linkedinPost(data)
+    def twitterPost(self, data):
+        
+    def facebookPost(self, data):
+
+    def linkedinPost(self, data):
+
