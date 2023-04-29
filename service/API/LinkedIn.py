b6a9aba616a17d19387dea28c6ac66dea6e4a74a
GeneDing <98688017+GeneDing@users.noreply.github.com>
2023-04-29 12:53:40 +0100

    LinkedIn api Init

--- service/API/LinkedIn.py
+++ service/API/LinkedIn.py
@@ -3,3 +3,12 @@ from flask import request
 from linkedin import linkedin
 from linkedin.linkedin import PERMISSIONS
 import json
+
+
+class LinkedIn:
+    def __init__(self):
+        with open("linkedin_auth.json") as json_file:
+            auth_dict = json.load(json_file)
+            access_token = auth_dict["access_token"]
+        self.access_token = access_token
+
