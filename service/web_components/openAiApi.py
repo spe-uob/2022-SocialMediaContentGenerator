6af2b5e38847d2bf036c5d47567b2a7e0709fe2c
Flgodd <benjyaram@gmail.com>
2023-05-01 18:15:08 +0100

    changes to openAiApi.py

--- service/web_components/openAiApi.py
+++ service/web_components/openAiApi.py
@@ -12,7 +12,7 @@ class openAiApi(Component):
     # when frontend request this api, this function will be called
     def view(self):
         apiKey = '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'
-       # apiK = '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'
+
         data = request.get_json()
         prompt = data['prompt']
         temp = data['temp']
@@ -23,25 +23,13 @@ class openAiApi(Component):
         openai.api_key = apiKey
 
         completion = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=[
-            {"role": "user", "content": prompt}
-          ],
-          temperature = temp
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            temperature=temp
         )
-
-        print("hreer")
-        print(completion.choices[0].message.content)
-        print("here")
 
         message = completion.choices[0].message.content
 
-
-
-
-
-
-
-
-        # you can reutrn a dict, it will be converted to json automatically
         return {'status': 'ok', 'text': message}
